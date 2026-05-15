508a614f36cc9809b3db49455cf67863bfd62cee
Kiran9991 <kiranagiwale@gmail.com>
2026-05-15 11:45:42 +0530

    completed personal form with ui

--- src/pages/ResumeForm.jsx
+++ src/pages/ResumeForm.jsx
@@ -3,46 +3,46 @@ import styles from './ResumeForm.module.css';
 
 export default function ResumeForm() {
   return (
-    <div className={styles.formContainer}>
-      <div className={styles.container1}>
+    <div className={styles.resumeForm}>
+      <div className={styles.formSection}>
         <form>
-            <div className={styles.formInput}>
+            <div className={styles.inputGroup}>
             <label >Full name</label>
             <input type='text' placeholder='Enter your Full name' />
             </div>
 
-            <div className={styles.formInput}>
+            <div className={styles.inputGroup}>
             <label >Email Address</label>
             <input type='email' placeholder='Enter your email address' />
             </div>
 
-            <div className={styles.formInput}>
+            <div className={styles.inputGroup}>
             <label >Phone Number</label>
             <input type='number' placeholder='Enter your phone number' />
             </div>
 
-            <div className={styles.formInput}>
+            <div className={styles.inputGroup}>
             <label >Location</label>
             <input type='text' placeholder='Enter your location' />
             </div>
 
-            <div className={styles.formInput}>
+            <div className={styles.inputGroup}>
             <label >Profession</label>
             <input type='text' placeholder='Enter your profession' />
             </div>
 
-            <div className={styles.formInput}>
+            <div className={styles.inputGroup}>
             <label >LinkedIn Profile</label>
             <input type='text' placeholder='Enter your linkedin profile' />
             </div>
 
-            <div className={styles.formInput}>
+            <div className={styles.inputGroup}>
             <label >Personal Website</label>
             <input type='text' placeholder='Enter your personal websie' />
             </div>
         </form>
       </div>
-      <div className={styles.container2}>
+      <div className={styles.previewSection}>
 
       </div>
     </div>
