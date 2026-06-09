57b6e34c119a94727d975703b03c6a185fdd0299
Kiran9991 <kiranagiwale@gmail.com>
2026-06-09 13:58:42 +0530

    added skill add functionality with display of list items

--- src/components/Resume form contents/Skills/Skills.jsx
+++ src/components/Resume form contents/Skills/Skills.jsx
@@ -2,38 +2,60 @@ import React, { useState } from "react";
 import FormTitle from "../Form/Form Title/FormTitle";
 import styles from "./Skills.module.css";
 import EmptyItems from "../Empty Items/EmptyItems";
-import { addform } from "../../../utils/addForm";
 
 export default function Skills() {
-    const [enteredSkill, setEnteredSkill] = useState('');
-    const [skills, setSkills] = useState([]);
+  const [enteredSkill, setEnteredSkill] = useState("");
+  const [skills, setSkills] = useState([]);
 
-    let content = <div>
-        {skills && skills.map((ele, idx) => (
+  const submitFormHandler = (e) => {
+    e.preventDefault();
+    if (!enteredSkill) return;
+    setSkills((prev) => [...prev, enteredSkill]);
+    setEnteredSkill("");
+  };
+
+  const deleteHandler = (id) => {
+    setSkills((prev) => [...prev.filter((ele, idx) => idx != id)]);
+  };
+
+  let content = (
+    <div className={styles.listSkillTag}>
+      {skills &&
+        skills.map((ele, idx) => (
+          <div key={idx} className={styles.skillTag}>
             <div>{ele}</div>
+            <button onClick={() => deleteHandler(idx)}>
+              <div className={styles.cross}>
+                <div className={styles.div1}></div>
+                <div className={styles.div2}></div>
+              </div>
+            </button>
+          </div>
         ))}
     </div>
+  );
 
-    if(skills.length == 0)
-      content = <EmptyItems text1={"skills"} text2={"add"} />
+  if (skills.length == 0)
+    content = <EmptyItems text1={"skills"} text2={"add"} />;
 
   return (
     <FormTitle
       title={"Skills"}
       description={"Add your technical and soft skills"}
     >
-      <div className={styles.inputContainer}>
+      <form className={styles.inputContainer} onSubmit={submitFormHandler}>
         <input
           type="text"
           placeholder="Enter a skill (e.g., JavaScript, Project Management)"
-          onChange={(e) => setEnteredSkill(e.targer.value)}
+          value={enteredSkill}
+          onChange={(e) => setEnteredSkill(e.target.value)}
         />
-        <button onClick={() => addform(skills, setSkills)}>Add</button>
-      </div>
+        <button type="submit">+Add</button>
+      </form>
       {content}
       <div className={styles.tipContainer}>
         <p>
-            <span>Tip:</span>
+          <span>Tip:</span>
           Add 8-12 relevant skills. Include both technical skills (programming
           languages, tools) and soft skills (leadership, communication).
         </p>
