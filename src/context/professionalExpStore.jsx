375e90642958c8fafdaafa3d0244069bf2e40afa
Kiran9991 <kiranagiwale3@gmail.com>
2026-07-08 19:36:22 +0530

    fixing the professional exp form with respect to resume id

--- src/context/professionalExpStore.jsx
+++ src/context/professionalExpStore.jsx
@@ -5,47 +5,21 @@ const ProfessionalExpContext = createContext();
 export default ProfessionalExpContext;
 
 export function ProfessionalExpProvider({ children }) {
-  const expDetailsArrObj = JSON.parse(localStorage.getItem("expDetails")) || [];
+  const [expdata, setExpdata] = useState([]);
 
-  const [expDetails, setExpDetails] = useState(expDetailsArrObj);
-
-  useEffect(() => {
-    localStorage.setItem("expDetails", JSON.stringify(expDetails));
-  }, [expDetails]);
-
-  const addFormHandler = () => {
-    setExpDetails((prev) => {
-      const newId = prev.length > 0 ? prev[prev.length - 1].id + 1 : 1;
-      return [
-        ...prev,
-        {
-          id: newId,
-          companyName: "",
-          jobTitle: "",
-          joiningDate: "",
-          endDate: "",
-          currentlyWorking: false,
-          jobDescription: "",
-        },
-      ];
-    });
-  };
+  const addFormHandler = (id) => {
+    setExpdata(
+      
+    };
 
   const updateExpDetailsFormHandler = (id, val) => {
-    setExpDetails((prev) =>
-      prev.map((item) => (item.id == id ? { ...item, ...val } : item)),
-    );
+
   };
 
   const deleteExpDetailsFormHandler = (id) => {
-    setExpDetails((prev) => [...prev.filter((item) => item.id !== id)]);
   };
 
   const expDetailsObj = {
-    expDetails: expDetails,
-    addForm: addFormHandler,
-    updateForm: updateExpDetailsFormHandler,
-    deleteForm: deleteExpDetailsFormHandler,
   };
 
   return (
